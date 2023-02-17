cfd25db14a6202b32e17a4f26046179b81506030
humEyra1064 <humeyraercanfurkanyilmaz@gmail.com>
2023-02-17 10:39:59 +0300

    card added

--- src/components/main/Card.jsx
+++ src/components/main/Card.jsx
@@ -1,9 +1,24 @@
 
 
-const Card = () => {
+const Card = (props) => {
+    const{id,title,date,desc,image} = props
   return (
-    <div className="card">
-      <h1>card</h1>
+    <div className="cards">
+         <div className="title">
+                <h1>{title}</h1>
+            </div>
+
+            <div className="date">
+                <h1>{date}</h1>
+            </div>
+
+            <img src={image} alt="" />
+            
+            <div className="card-over">
+                <p>
+                    {desc}
+                </p>
+            </div>
     </div>
   )
 }
